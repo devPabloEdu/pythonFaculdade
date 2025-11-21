e62c525aff23b379af72bf6fa3536d5fb2fec3f1
devpabloedu <vilelapege1113@gmail.com>
2025-11-20 21:59:46 -0300

    finalizando sessão do spark

--- spark/aula1spark.py
+++ spark/aula1spark.py
@@ -1,5 +1,10 @@
 from pyspark.sql import SparkSession
+
+# cria uma sessão do spark
 spark = SparkSession.builder.master("local[*]").appName("TesteInstalaçãoSpark").getOrCreate();
 
 dataset = spark.read.csv('/sample_data_california_housing_test.csv', inferSchema=True, header=True);
 dataset.printSchema();
+
+#finaliza a sessão do spark
+spark.stop();
