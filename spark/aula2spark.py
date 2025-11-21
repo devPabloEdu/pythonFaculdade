9524f8c9544198dad7bec36cca7585a4272475d5
devpabloedu <vilelapege1113@gmail.com>
2025-11-20 22:56:16 -0300

    convertendo para o Pandas e manipulação de dataframes

--- spark/aula2spark.py
+++ spark/aula2spark.py
@@ -18,3 +18,16 @@ print(spark_session.catalog.listTables());
 query = 'FROM tabela_temporaria SELECT longitude, latitude LIMIT 3';
 saida = spark_session.sql(query);
 saida.show();
+
+query1 = 'SELECT MAX(total_roms) as maximo_quartos FROM tabela_temporaria';
+q_maximo_quartos = spark_session.sql(query1); #roda a query
+panda_maximo_quartos = q_maximo_quartos.toPandas(); #converte a tabela para o formato do pandas
+print('A quantidade máxima de quartos é: {}'.format(panda_maximo_quartos['maximo_quartos'])); #faz a interpolação com o format
+quantidade_maxima_quartos = int(panda_maximo_quartos.loc[0,'maximo_quartos']); #converte para um inteiro
+
+query2 = 'SELECT longitude, latitude FROM tabela_temporaria WHERE total_roms = '+str(q_maximo_quartos);
+localizacao_maximo_quartos = spark_session.sql(query2);
+pandas_localizacao_maximo_quartos = localizacao_maximo_quartos.toPandas();
+print(pandas_localizacao_maximo_quartos.head());
+
+
